db1021e3225656526c2e16cc7503815d72d19409
Mahfuz <you@example.com>
2021-04-05 23:34:16 +0600

    code

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const client = new MongoClient(uri, {
 });
 const app = express();
 app.use(cors());
+
 app.use(express.json());
 console.log(process.env.DB_USER,process.env.DB_PASS,process.env.DB_NAME)
 
